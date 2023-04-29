135e3a1d92b24c27ff15b8a17fd41c6e68df7bce
VictoriaYotka <victoria.borisyuk@gmail.com>
2023-04-29 07:29:38 +0300

    render statistics (no styles)

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,25 @@
 import{ Profile } from './profile/Profile';
 import{ Stats } from './profile/Stats';
+import{ Statistics } from './statistics/Statistics';
 import user from '../user.json';
+import data from '../data.json';
 
 export const App = () => {
   return (
     <>
       <Profile
-        username={user.username}
-        tag={user.tag}
-        location={user.location}
-        avatar={user.avatar}
+          username={user.username}
+          tag={user.tag}
+          location={user.location}
+          avatar={user.avatar}
       />
 
       <Stats stats={user.stats} />
+
+      <Statistics 
+        title='Upload stats'
+        stats={data}
+      />
     </>
   );
 };
---
+++ src/components/statistics/Statistics.jsx
@@ -0,0 +1,14 @@
+export const Statistics = ({ title, stats }) => {
+    return (
+        <section>
+        {title.length > 0 && (<h2>{title}</h2>)}
+        
+        <ul>
+            {stats.map(stat => <li key={stat.id}>
+                <span>{stat.label}</span>
+                <span>{stat.percentage}</span>
+            </li>)}
+        </ul>
+        </section>
+    )
+}
