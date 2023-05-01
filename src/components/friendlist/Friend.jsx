6846c7309e6755cbb64d5b43d993d20459c027b6
VictoriaYotka <victoria.borisyuk@gmail.com>
2023-05-01 14:17:42 +0300

    add styles to friendslist

--- src/components/friendlist/Friend.jsx
+++ src/components/friendlist/Friend.jsx
@@ -1,3 +1,5 @@
+import css from './Friend.module.css'
+
 export const Friend = ( friend ) => {
     const { 
         avatar = 'https://cdn-icons-png.flaticon.com/512/2922/2922506.png',
@@ -6,9 +8,9 @@ export const Friend = ( friend ) => {
     } = friend;
     
     return (
-    <li>
-        <span>{isOnline}</span>
-        <img src={avatar} alt="User avatar" width="48" />
-        <p>{name}</p>
+    <li className={css.item}>
+        <span className={isOnline ? css.online : css.offline}></span>
+        <img className={css.avatar} src={avatar} alt="User avatar" width="48" />
+        <p className={css.name}>{name}</p>
     </li>
 )};
--- src/components/friendlist/Friendlist.jsx
+++ src/components/friendlist/Friendlist.jsx
@@ -1,7 +1,8 @@
 import { Friend } from "./Friend";
+import css from './Friendlist.module.css'
 
 export const Friendlist = ({friends}) => (
-    <ul>
+    <ul className={css.friend_list}>
         {friends.map(friend => <Friend {...friend} key={friend.id} /> )}
         
     </ul>
